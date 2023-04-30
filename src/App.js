530f6720179068552f78562fe1bbead1cb28053d
salvagebar <salvagebar@gmail.com>
2023-04-30 10:24:00 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import dice from './greekdie500.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={dice} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
